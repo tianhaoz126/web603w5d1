d3310e409010880900735a5c5f10d54c601e25eb
Eric Zhou <ericzhou1212@gmail.com>
2024-07-29 18:59:46 -0700

    completed

--- m6_week5_d1/factory/gadgetFactory.js
+++ m6_week5_d1/factory/gadgetFactory.js
@@ -4,3 +4,9 @@ const Tablet = require("./tablet");
 const gadget = { Laptop, Tablet };
 
 // add module.export here
+module.exports={
+    createGadget(type, attribtues){
+        const GadgetType = gadget[type];
+        return new GadgetType(attribtues);
+    }
+};
